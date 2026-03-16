05be0cf527b427755274098587c44fc4b2b04b45
John Doe <john.doe@gmail.com>
2026-03-16 16:24:30 +0300

    Final Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import Footer from "./components/footer.jsx";
 import Hero from "./components/hero.jsx";
-import Order from "./components/Order.jsx";
+import Order from "./components/order.jsx";
 import Products from "./components/products.jsx";
 import ProductOrder from "./components/productOrder.jsx";
 import About from "./components/About.jsx";
